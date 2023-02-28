269a2cb350c75e7b0933c04973e82868fee43056
salimzeeshan <zeeshasalim541@gmail.com>
2023-02-28 20:17:48 +0530

    Updated home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8145d31ea87823a"; }
+/******/ 		__webpack_require__.h = function() { return "5f81457b3d8b2fbf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <Home></Home>
+      <div></div>
     </>
   )
 }
