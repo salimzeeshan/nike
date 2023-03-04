c65d6c1965e55d78450d8b2fc2667d58afb2dd8b
salimzeeshan <zeeshasalim541@gmail.com>
2023-03-04 22:22:52 +0530

    Men's page setup

--- nike/src/pages/men/shoes/index.jsx
+++ nike/src/pages/men/shoes/index.jsx
@@ -1,57 +1,68 @@
 import { Box, filter, Flex, Text } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BsFilterRight } from "react-icons/bs";
 
 function MenShoes() {
-  useEffect(() => {
-    const hideFilter = document.querySelector(".hide-filter");
+  const [hide, setHide] = useState(false);
 
-    hideFilter.addEventListener("click", () => {
-      const filters = document.querySelector(".filters");
-      const products = document.querySelector(".products");
-      const home = document.querySelector(".home");
-      const data_id = home.getAttribute("data-filters");
-      console.log(data_id);
+  const handleFilter = () => {
+    const filters = document.querySelector(".filters");
+    const products = document.querySelector(".products");
+    const home = document.querySelector(".home");
+    const data_id = home.getAttribute("data-filters");
+    console.log(data_id);
 
-      if (data_id == "false") {
-        filters.classList.add("filters-hide");
-        products.classList.add("products-full");
-        home.setAttribute("data-filters", "true");
-      }
-      if (data_id == "true") {
-        filters.classList.remove("filters-hide");
-        products.classList.remove("products-full");
-        home.setAttribute("data-filters", "false");
-      }
-    });
-  }, []);
+    if (data_id == "false") {
+      filters.classList.add("filters-hide");
+      products.classList.add("products-full");
+      home.setAttribute("data-filters", "true");
+      setHide(!hide);
+    }
+    if (data_id == "true") {
+      filters.classList.remove("filters-hide");
+      products.classList.remove("products-full");
+      home.setAttribute("data-filters", "false");
+      setHide(!hide);
+    }
+  };
 
   return (
-    <Box data-filters="false" className="home" mt={"50px"}>
+    <Box
+      data-filters="false"
+      className="home"
+      mt={"50px"}>
       <Flex
         py={"10px"}
         w={"100%"}
         alignItems="center"
         justifyContent="space-between"
-        border="1px solid black"
         mb={"20px"}>
         <Text fontWeight={"bold"} fontSize={"3xl"}>
           Men's Shoes & Sneakers
         </Text>
         <Flex>
-          <Flex className="hide-filter" alignItems={"center"}>
-            <Text>Hide Filters</Text>
+          <Flex
+            cursor={"pointer"}
+            onClick={handleFilter}
+            className="hide-filter"
+            alignItems={"center"}>
+            {hide ? <Text>Show Filters</Text> : <Text>Hide Filters</Text>}
             <BsFilterRight />
           </Flex>
         </Flex>
       </Flex>
-      <Flex className="product-container" justifyContent={"end"} gap={"30px"}>
-        <Flex h={"1000px"} className="filters">
+      <Flex
+        position={"relative"}
+        width={"100%"}
+        className="product-container"
+        gap={"30px"}>
+        <Box className="dummy-div"></Box>
+        <Box h={"1000px"} className="filters">
           Filters
-        </Flex>
-        <Flex h={"1000px"} className="products">
+        </Box>
+        <Box h={"1000px"} className="products">
           Products
-        </Flex>
+        </Box>
       </Flex>
     </Box>
   );
