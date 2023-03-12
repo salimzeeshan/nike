10f59622afa4445e86ea2ff552ed42cb9686c530
salimzeeshan <zeeshasalim541@gmail.com>
2023-03-12 22:18:28 +0530

    Added user data in the database

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express()
-
 const app = express()
-app.use(express.json())
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 const productController = require('../controllers/productController')
 
