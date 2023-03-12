678cd8eff081540f8358c292b3faa2eff3ab0058
salimzeeshan <zeeshasalim541@gmail.com>
2023-03-12 16:47:46 +0530

    Added user data in the database

--- controllers/productController.js
+++ controllers/productController.js
@@ -88,7 +88,7 @@ const accessories = async (req, res, next) => {
 
 const users = async (req, res, next) => {
     try {
-        const data = await productModel.user.find({ "email": `${req.email}` })
+        const data = await productModel.user.find()
         console.log(req.body)
         res.send(data)
     } catch (error) {
