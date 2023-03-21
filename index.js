ce18f12227c7eadec999a5d17f7308fcd385b75d
salimzeeshan <zeeshasalim541@gmail.com>
2023-03-21 18:58:36 +0530

    Added environment variables

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const router = require('./routes/productRoutes')
-const database = 'mongodb+srv://zeeshansalim:456123@nike-products.shrdb01.mongodb.net/nike?retryWrites=true&w=majority'
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const router = require("./routes/productRoutes");
+const database = process.env.MONGO_DB;
 
-app.use(cors())
+app.use(cors());
 
-const main = () => { 
-    mongoose.connect(database)
-}
+const main = () => {
+  mongoose.connect(database);
+};
 
 app.listen(2080, async (req, res) => {
-    try {
-        await main()
-        console.log("Connected to the database successfully")
-    } catch (error) {
-        console.log(error)
-    }
-    console.log("Port: 2080")
-})
+  try {
+    await main();
+    console.log("Connected to the database successfully");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("Port: 2080");
+});
 
-app.use('/', router)
+app.use("/", router);
